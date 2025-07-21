d3efa8f031f465209f48f8fbca879c13ebba8cc3
Kleber Felipe <kleberfca@gmail.com>
2025-07-21 19:38:33 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 // src/App.tsx
-/**
- * Componente principal da aplicação
- * Define rotas e estrutura geral
- */
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
@@ -35,101 +30,103 @@ function App() {
           <NotificationProvider>
             <Layout>
               <Routes>
-            {/* Rota pública */}
-            <Route path="/login" element={<Login />} />
+                {/* Rota pública */}
+                <Route path="/login" element={<Login />} />
 
-            {/* Rotas do administrador */}
-            <Route
-              path="/admin/dashboard"
-              element={
-                <PrivateRoute requireAdmin>
-                  <AdminDashboard />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/clientes"
-              element={
-                <PrivateRoute requireAdmin>
-                  <ClientesList />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/clientes/novo"
-              element={
-                <PrivateRoute requireAdmin>
-                  <ClienteForm />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/clientes/:id"
-              element={
-                <PrivateRoute requireAdmin>
-                  <ClienteForm />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/projetos"
-              element={
-                <PrivateRoute requireAdmin>
-                  <ProjetosList />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/projetos/novo"
-              element={
-                <PrivateRoute requireAdmin>
-                  <ProjetoForm />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/projetos/:id"
-              element={
-                <PrivateRoute requireAdmin>
-                  <ProjetoDetails />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/projetos/:id/editar"
-              element={
-                <PrivateRoute requireAdmin>
-                  <ProjetoForm />
-                </PrivateRoute>
-              }
-            />
+                {/* Rotas do administrador */}
+                <Route
+                  path="/admin/dashboard"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <AdminDashboard />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin/clientes"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <ClientesList />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin/clientes/novo"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <ClienteForm />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin/clientes/:id"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <ClienteForm />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin/projetos"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <ProjetosList />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin/projetos/novo"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <ProjetoForm />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin/projetos/:id"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <ProjetoDetails />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin/projetos/:id/editar"
+                  element={
+                    <PrivateRoute requireAdmin>
+                      <ProjetoForm />
+                    </PrivateRoute>
+                  }
+                />
 
-            {/* Rotas do cliente */}
-            <Route
-              path="/cliente/projetos"
-              element={
-                <PrivateRoute>
-                  <ProjetosCliente />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/cliente/projetos/:id"
-              element={
-                <PrivateRoute>
-                  <ProjetoDetailsCliente />
-                </PrivateRoute>
-              }
-            />
+                {/* Rotas do cliente */}
+                <Route
+                  path="/cliente/projetos"
+                  element={
+                    <PrivateRoute>
+                      <ProjetosCliente />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/cliente/projetos/:id"
+                  element={
+                    <PrivateRoute>
+                      <ProjetoDetailsCliente />
+                    </PrivateRoute>
+                  }
+                />
 
-            {/* Redirecionamentos */}
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="/admin" element={<Navigate to="/admin/dashboard" replace />} />
-            <Route path="/cliente" element={<Navigate to="/cliente/projetos" replace />} />
-          </Routes>
-        </Layout>
-      </AuthProvider>
-    </Router>
+                {/* Redirecionamentos */}
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/admin" element={<Navigate to="/admin/dashboard" replace />} />
+                <Route path="/cliente" element={<Navigate to="/cliente/projetos" replace />} />
+              </Routes>
+            </Layout>
+          </NotificationProvider>
+        </AuthProvider>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
